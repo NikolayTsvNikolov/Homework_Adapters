90e90283d30190a7d229392a0bf44f49adee0a41
Nikolay Nikolov <nikolay.nikolov92@gmail.com>
2016-01-20 16:17:20 +0200

    Add Button aactions

--- app/src/main/java/com/example/toshiba/myapplication/AddComment.java
+++ app/src/main/java/com/example/toshiba/myapplication/AddComment.java
@@ -1,37 +1,62 @@
 package com.example.toshiba.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class AddComment extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class AddComment extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.list_view)
+    ListView listView;
+    @Bind(R.id.txt_add_comment)
+    EditText txtAddComment;
+    @Bind(R.id.btn_add_comment)
+    Button btnAddComment;
+    List<String> arrList = new ArrayList<>();
+    private ArrayAdapter<String> arrAdapter;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comment);
+        ButterKnife.bind(this);
+        position = getIntent().getIntExtra("position", 0);
+        arrAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_dropdown_item_1line, arrList);
+        listView.setAdapter(arrAdapter);
+        btnAddComment.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        arrAdapter.add(txtAddComment.getText().toString());
+//        arrList.add(txtAddComment.getText().toString());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_comment, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed() {
+//        super.onBackPressed();
+        int count = arrList.size();
+        Intent intent = new Intent();
+        intent.putExtra("countComment", count);
+        intent.putExtra("position", position);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/toshiba/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/toshiba/myapplication/MyAdapter.java
@@ -1,7 +1,137 @@
 package com.example.toshiba.myapplication;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static com.example.toshiba.myapplication.R.*;
+
 /**
  * Created by toshiba on 7.1.2016 г..
  */
-public class MyAdapter {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+
+    private List<Event> events = new ArrayList<>();
+
+    OnEventClicListener listener;
+
+
+    public MyAdapter(List<Event> events, OnEventClicListener listener) {
+        this.listener = listener;
+        this.events = events;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(layout.list_item_ivent, parent, false);
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.position = position;
+        holder.txtTitle.setText(events.get(position).titleId);
+        holder.numComments.setText(events.get(position).numComments);
+        holder.img.setImageResource(events.get(position).imgId);
+        holder.numPoints.setText(events.get(position).numPoints);
+        holder.btnPointsUp.setBackgroundResource(events.get(position).pointsUpId);
+        holder.btnPointsDown.setBackgroundResource(events.get(position).pointsDownId);
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return events.size();
+    }
+
+    public Event getEvent(int position) {
+
+        return events.get(position);
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @Bind(id.btn_share)
+        Button btnShare;
+        @Bind(id.btn_points_up)
+        Button btnPointsUp;
+        @Bind(id.btn_points_down)
+        Button btnPointsDown;
+        @Bind(id.btn_comment)
+        Button btnComment;
+        @Bind(id.img)
+        ImageView img;
+        @Bind(id.txt_comments)
+        TextView numComments;
+        @Bind(id.txt_points)
+        TextView numPoints;
+        @Bind(id.txt_title)
+        TextView txtTitle;
+        int position;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            btnPointsUp.setOnClickListener(this);
+            btnPointsDown.setOnClickListener(this);
+            btnComment.setOnClickListener(this);
+            btnShare.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+
+                case id.btn_comment:
+                    listener.onCommentClicked(position);
+                    break;
+
+                case id.btn_points_up:
+                    listener.onPointClicked(true, position);
+                    break;
+
+                case id.btn_points_down:
+                    listener.onPointClicked(false, position);
+                    break;
+
+                case id.btn_share:
+                    listener.onShareClicked();
+                    break;
+            }
+
+
+        }
+    }
+
+
+    public interface OnEventClicListener {
+
+        void onShareClicked();
+
+        void onPointClicked(boolean is, int position);
+
+        void onCommentClicked(int position);
+
+
+    }
+
 }
+
+
